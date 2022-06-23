9577509274cad24dece63d23e39bcf0a9e5b1671
Rutvik Navadiya <103628438+rutvik582000@users.noreply.github.com>
2022-06-23 15:57:59 +0530

    printed all news by map function

--- src/Component/News.js
+++ src/Component/News.js
@@ -2,11 +2,19 @@ import React from "react";
 import NewsItems from "./NewsItems";
 import sample from "./sample-response.json";
 
-
 export default function News() {
   return (
-    <div>      
-      <NewsItems articles = {sample.articles} />
+    <div className="container my-3">
+      <h1 className="text-center">Read news about world here</h1>
+      <div className="row">
+        {sample.articles.map((article) => {
+          return (
+            <div className="col-md-4">
+              <NewsItems article={article} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/Component/NewsItems.js
+++ src/Component/NewsItems.js
@@ -2,19 +2,19 @@ import React from "react";
 
 
 export default function NewsItems(props) {
+  {console.log(props.article);}
   return (
-    <div className="container my-3">
-        <h1 className="text-center">Read news about wotld here</h1>
+    
       <div className="card my-3" style={{ width: "18rem" }}>
-        <img src= {props.articles[0].urlToImage}  className="card-img-top" alt="..." />
+        <img src= {props.article.urlToImage}  className="card-img-top" alt="img" />
         <div className="card-body">
-          <h5 className="card-title">{props.articles[0].title}</h5>
+          <h5 className="card-title">{props.article.title}</h5>
           <p className="card-text">
-            {props.articles[0].description}            
+            {props.article.description}            
           </p>
-          <a href={props.articles[0].url} target="_blank" rel="noreferrer noopener" className="btn btn-primary">Read more...</a>
+          <a href={props.article.url} target="_blank" rel="noreferrer noopener" className="btn btn-primary">Read more...</a>
         </div>
       </div>
-    </div>
+
   );
 }
